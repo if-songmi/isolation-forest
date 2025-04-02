dc3109b022dd9072243675b207e04217a3e4480a
songmi <57933306+if-songmi@users.noreply.github.com>
2025-04-02 03:01:47 +0000

    Add code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,3 @@ from streamlit_javascript import st_javascript
 import time
 
 st.subheader("Isolation Forest", divider='gray')
-while True:
-    PassingData = st_javascript('parent.window.PassingData')
-    if PassingData:
-        print(PassingData);
-        break
-    time.sleep(1)
-
