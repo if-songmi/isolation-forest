d6890f93b69dde2b0e0f6670aa07f2c26f10245a
songmi <57933306+if-songmi@users.noreply.github.com>
2025-04-03 00:53:09 +0000

    Add data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 from streamlit_javascript import st_javascript
-import time
 
 st.subheader("Isolation Forest", divider='gray')
+df = pd.read_json("data.json")
+st.dataframe(df, hide_index = True)
